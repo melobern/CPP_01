f97ff7fed0d0790a1c38a8e47291434726e92ca1
mbernard <melodie.bernard123@yahoo.fr>
2024-07-28 17:20:54 +0200

    init files

---
+++ ex00/includes/Zombie.hpp
@@ -0,0 +1,19 @@
+/* Copyright 2024 <mbernard>************************************************* */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 17:17:15 by mbernard          #+#    #+#             */
+/*   Updated: 2024/07/28 17:17:24 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EX00_INCLUDES_ZOMBIE_HPP_
+#define EX00_INCLUDES_ZOMBIE_HPP_
+
+#include <iostream>
+#include <string>
+
+#endif  // EX00_INCLUDES_ZOMBIE_HPP_
