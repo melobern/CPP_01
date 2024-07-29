50dc5fb99c37b4ccf23e5e18570465dbb73688c1
mbernard <mbernard@student.42lyon.fr>
2024-07-29 19:19:37 +0200

    EX 02, added tabs to align the out

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -19,11 +19,11 @@ int main(void) {
     std::string *stringPTR = &str;
     std::string &stringREF = str;
 
-    std::cout << "Address of the string:\t" << &str << std::endl;
+    std::cout << "Address of the string:\t\t" << &str << std::endl;
     std::cout << "Address of the stringPTR:\t" << stringPTR << std::endl;
     std::cout << "Address of the stringREF:\t" << &stringREF << std::endl;
-    std::cout << "Value of the string:\t" << str << std::endl;
-    std::cout << "Value of the stringPTR:\t" << *stringPTR << std::endl;
-    std::cout << "Value of the stringREF:\t" << stringREF << std::endl;
+    std::cout << "Value of the string:\t\t" << str << std::endl;
+    std::cout << "Value of the stringPTR:\t\t" << *stringPTR << std::endl;
+    std::cout << "Value of the stringREF:\t\t" << stringREF << std::endl;
     return (0);
 }
