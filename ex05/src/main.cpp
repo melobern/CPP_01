ab73904556ebcb50bd2c90652987ea1e79243482
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-13 10:00:06 +0200

    begin harl_2.0 + removed crap from ex04 main

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -5,31 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/28 17:15:23 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/12 16:15:18 by mbernard         ###   ########.fr       */
+/*   Created: 2024/08/13 09:17:56 by mbernard          #+#    #+#             */
+/*   Updated: 2024/08/13 09:58:43 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "../includes/FileSrc.hpp"
+#include "../includes/Harl.hpp"
 
-/*
-    Exercise 04: Sed is for losers
-    Exercise : 04
-    Sed is for losers
-    Turn-in directory : ex04/
-    Files to turn in : Makefile, main.cpp, *.cpp, *.{h, hpp}
-    Forbidden functions : std::string::replace
-    Create a program that takes three parameters in the following order: a filename and
-    two strings, s1 and s2.
-    It will open the file <filename> and copies its content into a new file
-    <filename>.replace, replacing every occurrence of s1 with s2.
-    Using C file manipulation functions is forbidden and will be considered cheating. All
-    the member functions of the class std::string are allowed, except replace. Use them
-    wisely!
-    Of course, handle unexpected inputs and errors. You have to create and turn in your
-    own tests to ensure your program works as expected.
- */
 int main(int ac, char **av) {
     if (ac != 4) {
         std::cout << "Error: wrong number of arguments" << std::endl;
