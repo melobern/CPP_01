43944328429c7932240c9eecc07496648e9afb0c
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-13 11:06:05 +0200

    finished Harl2.0 and cpplint it

--- ex05/includes/Harl.hpp
+++ ex05/includes/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 09:17:56 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/13 09:25:58 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/13 11:05:34 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 
 class Harl {
  public:
-   Harl(void);
-   ~Harl();
-   void complain(std::string level);
+    Harl(void);
+    ~Harl(void);
+    void complain(std::string level);
  private:
-   void debug(void);
-   void info(void);
-   void warning(void);
-   void error(void);
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
 };
 #endif  // EX05_INCLUDES_HARL_HPP_
