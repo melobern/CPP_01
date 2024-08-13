757d49e2d15180ea21732492ca2628c044c0d32e
Melodie Bernard (from school) <mbernard@z3r8p3.42lyon.fr>
2024-08-13 11:16:11 +0200

    added randomtest function

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -6,12 +6,20 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 09:17:56 by mbernard          #+#    #+#             */
-/*   Updated: 2024/08/13 11:07:35 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/08/13 11:15:41 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#include <cstdlib>
 #include "../includes/Harl.hpp"
+
+static void randomComplain(Harl harl) {
+    std::string complain[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+    for (int i = 0; i < 10; i++)
+        harl.complain(complain[rand() % 3]);
+}
 
 int main(void) {
     Harl harl;
@@ -20,5 +28,6 @@ int main(void) {
     harl.complain("INFO");
     harl.complain("WARNING");
     harl.complain("ERROR");
+    randomComplain(harl);
     return 0;
 }
